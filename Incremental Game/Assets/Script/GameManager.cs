71cc1e97219a6b707f318f49ce96ca2088f58e1f
KewpieBinar <70208514+KewpieBinar@users.noreply.github.com>
2021-04-10 17:40:57 +0700

    Finished Game

--- Incremental Game/Assets/Script/GameManager.cs
+++ Incremental Game/Assets/Script/GameManager.cs
@@ -25,6 +25,11 @@ public class GameManager : MonoBehaviour
     [Range(0f, 1f)]
     public float AutoCollectPercentage = 0.1f;
     public ResourceConfig[] ResourceConfigs;
+    public Sprite[] ResourcesSprites;
+
+    public TapText TapTextPrefab;
+
+    public Transform CoinIcon;
 
     public GameObject ResourcePrefab;
     public Transform ResourceParent;
@@ -34,7 +39,10 @@ public class GameManager : MonoBehaviour
     private List<ResourceController> _activeResources = new List<ResourceController>();
     private float _collectTimer;
 
+    private List<TapText> _tapTextPool = new List<TapText>();
+
     private double _totalGold;
+    public double TotalGold => _totalGold;
 
     private void Start()
     {
@@ -49,6 +57,12 @@ public class GameManager : MonoBehaviour
             CollectPerSecond();
             _collectTimer = 0;
         }
+
+        CheckResourceCost();
+
+        CoinIcon.transform.localScale = Vector3.LerpUnclamped(CoinIcon.transform.localScale, Vector3.one * 2f, 0.15f);
+
+        CoinIcon.transform.Rotate(0f, 0f, Time.deltaTime * -100f);
     }
 
     private void CollectPerSecond()
@@ -56,6 +70,7 @@ public class GameManager : MonoBehaviour
         double output = 0;
         foreach(ResourceController resource in _activeResources)
         {
+            if(resource.IsUnlocked)
             output += resource.GetOutput();
         }
 
@@ -67,7 +82,7 @@ public class GameManager : MonoBehaviour
 
     }
 
-    private void AddGold(double value)
+    public void AddGold(double value)
     {
         _totalGold += value;
         GoldInfo.text = $"Gold: {_totalGold.ToString("0")}";
@@ -75,15 +90,112 @@ public class GameManager : MonoBehaviour
 
     private void AddAllResources()
     {
+
+        bool showResources = true;
+
         foreach(ResourceConfig config in ResourceConfigs)
         {
+            
             GameObject obj = Instantiate(ResourcePrefab.gameObject, ResourceParent, false);
             ResourceController resource = obj.GetComponent<ResourceController>();
 
             resource.SetConfig(config);
+
+            obj.gameObject.SetActive(showResources);
+
+            if(showResources && !resource.IsUnlocked)
+            {
+                showResources = false;
+            }
+
             _activeResources.Add(resource);
 
         }
+    }
+
+    public void ShowNextResources()
+    {
+        foreach(ResourceController resource in _activeResources)
+        {
+            if(!resource.gameObject.activeSelf)
+            {
+                resource.gameObject.SetActive(true);
+                break;
+            }
+        }
+    }
+
+    private void CheckResourceCost()
+    {
+        foreach(ResourceController resource in _activeResources)
+        {
+            bool isBuyable = false;//= TotalGold >= resource.GetUpgradeCost();
+
+            if(resource.IsUnlocked)
+            {
+                isBuyable = TotalGold >= resource.GetUpgradeCost();
+            }
+            else
+            {
+                isBuyable = TotalGold >= resource.GetUnlockCost();
+            }
+            resource.ResourceImage.sprite = ResourcesSprites[isBuyable ? 1 : 0];
+        }
+    }
+
+    public void CollectByTap (Vector3 tapPosition, Transform parent)
+    {
+        double output = 0;
+
+        foreach (ResourceController resource in _activeResources)
+        {
+
+            if(resource.IsUnlocked)
+                output += resource.GetOutput();
+
+        }
+
+
+
+        TapText tapText = GetOrCreateTapText();
+
+        tapText.transform.SetParent(parent, false);
+
+        tapText.transform.position = tapPosition;
+
+        tapText.TextToTap.text = $"+{ output.ToString("0") }";
+
+        tapText.gameObject.SetActive(true);
+
+        CoinIcon.transform.localScale = Vector3.one * 1.75f;
+
+
+
+        AddGold(output);
+    }
+
+    private TapText GetOrCreateTapText()
+    {
+
+        TapText tapText = _tapTextPool.Find(t => !t.gameObject.activeSelf);
+
+        if (tapText == null)
+
+        {
+
+            tapText = Instantiate(TapTextPrefab).GetComponent<TapText>();
+            
+
+            _tapTextPool.Add(tapText);
+
+            
+
+        }
+
+
+
+        return tapText;
+
     }
 }
 
--- Incremental Game/Assets/Script/ResourceController.cs
+++ Incremental Game/Assets/Script/ResourceController.cs
@@ -5,6 +5,9 @@ using UnityEngine.UI;
 
 public class ResourceController : MonoBehaviour
 {
+    public Button ResourceButton;
+    public Image ResourceImage;
+
     public Text ResourceDescription;
     public Text ResourceUnlockCost;
     public Text ResourceUpgradeCost;
@@ -12,7 +15,25 @@ public class ResourceController : MonoBehaviour
     private ResourceConfig _config;
 
     private int _level = 1;
-    
+
+    public bool IsUnlocked { get; private set; }
+
+    private void Start()
+    {
+        ResourceButton.onClick.AddListener(
+            () => { 
+                if (IsUnlocked)
+                {
+                    UpgradeLevel();
+                }
+                else
+                {
+                    UnlockResources();
+                }
+            });
+        ResourceButton.onClick.AddListener(UpgradeLevel);
+    }
+
     public void SetConfig(ResourceConfig config)
     {
         _config = config;
@@ -21,6 +42,7 @@ public class ResourceController : MonoBehaviour
         ResourceUnlockCost.text = $"Unlock Cost\n{_config.UnlockCost}";
         ResourceUpgradeCost.text = $"Upgrade Cost\n{GetUpgradeCost()}";
 
+        SetUnlocked(_config.UnlockCost == 0);
     }
 
     public double GetOutput()
@@ -36,6 +58,48 @@ public class ResourceController : MonoBehaviour
     public double GetUnlockCost()
     {
         return _config.UnlockCost;
+    }
+
+    public void UpgradeLevel()
+    {
+        double upgradeCost = GetUpgradeCost();
+
+        if (GameManager.Instance.TotalGold < upgradeCost)
+        {
+
+            return;
+
+        }
+
+        GameManager.Instance.AddGold(-upgradeCost);
+        _level++;
+
+
+
+        ResourceUpgradeCost.text = $"Upgrade Cost\n{ GetUpgradeCost() }";
+        ResourceDescription.text = $"{ _config.Name } Lv. { _level }\n+{ GetOutput().ToString("0") }";
+    }
+
+    public void UnlockResources()
+    {
+        double unlockCost = GetUnlockCost();
+        if (GameManager.Instance.TotalGold<unlockCost)
+        {
+            return;
+        }
+
+        SetUnlocked(true);
+        AchievementController.Instance.UnlockAchievement(AchievementType.UnlockResource, _config.Name);
+        GameManager.Instance.ShowNextResources();
+    }
+
+    public void SetUnlocked (bool unlocked)
+    {
+        IsUnlocked = unlocked;
+        ResourceImage.color = IsUnlocked ? Color.white : Color.grey;
+        ResourceUnlockCost.gameObject.SetActive(!unlocked);
+        ResourceUpgradeCost.gameObject.SetActive(unlocked);
+
     }
 
     private void OnMouseDown()
